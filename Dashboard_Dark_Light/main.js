4f3188d229163c87399f40594129cc72aa422d9e
Pedro <pedroegc692@gmail.com>
2022-07-18 00:22:57 -0300

    primeiro commit

---
+++ Dashboard_Dark_Light/main.js
@@ -0,0 +1,56 @@
+const chart = document.querySelector("#chart").getContext('2d');
+
+new Chart(chart, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul',
+        'Ago', 'Set', 'Out', 'Nov'],
+
+        datasets: [
+            {
+                label: 'BTC',
+                data: [29734, 33537, 49631, 59095, 57828, 36684,
+                33572, 39974, 48847, 48116, 61004],
+                borderColor: 'red',
+                borderWidth: 2
+            },
+            {
+                label: 'ETH',
+                data: [31500, 41000, 88800, 26000, 46000, 32698,
+                5000, 3000, 18656, 24832, 36844],
+                borderColor: 'blue',
+                borderWidth: 2
+            }
+        ]
+    },
+    options: {
+        responsive: true
+    }
+})
+
+// Menu Lateral 
+
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () => {
+    sidebar.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sidebar.style.display = 'none';
+})
+
+
+// Mudança de Tema
+
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+})
